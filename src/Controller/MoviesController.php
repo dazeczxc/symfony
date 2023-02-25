3da4d22cba4d5ee75c9dd96322b0acf9f0fc946b
dazec <dasecojoshua@gmail.com>
2023-02-24 19:15:49 -0800

    Make some expirements on how to query data from database

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'movies')]
-    public function index(MovieRepository $movieRepository): Response
+
+    private $em;
+    public function __construct(EntityManagerInterface $em)
     {
-        $movies = $movieRepository->findAll(); //getting all data in the database
+        $this->em = $em;
+    }
+
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
+    {
+        //findAll() - SELECT * FROM TABLENAME;
+        //find() - SELECT * FROM TABLENAME WHERE CONDITIONS;
+        //findBy - SELECT * FROM TABLENAME ORDER BY ASC/DESC;
+        //findOneBy - SELECT * FROM TABLENAME WHERE CONDITIONS ORDER BY ASC/DESC;
+        //count() - SELECT COUNT() FROM TABLENAME WHERE CONDITIONS;
+
+        $repository = $this->em->getRepository(Movie::class);
+
+        $movies = $repository->getClassName();
+
         dd($movies);
         return $this->render('index.html.twig');
     }
+
 }
